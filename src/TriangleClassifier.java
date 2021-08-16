d872d33cc459f0c0dcf28f76c14ac0f5f31fada0
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-08-16 17:30:29 +0700

    first commit

---
+++ src/TriangleClassifier.java
@@ -0,0 +1,34 @@
+public class TriangleClassifier {
+
+    public static final String NOTTRIANGLE = "Day khong phai tam giac";
+    public static final String TRIANGLEEQUILATERAL = "Tam giac deu";
+    public static final String TRIANGLEISOSCELES = "Tam giac can";
+    public static final String NORMALTRIANGLE = "Tam giac thuong";
+
+    public static String classifyTriangle(int sideA, int sideB, int sideC) {
+        boolean sideAEqualB = sideA == sideB;
+        boolean sideAEqualC = sideA == sideC;
+        boolean sideBEqualC = sideB == sideC;
+        String result = "";
+        if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA)) {
+            result = NOTTRIANGLE;
+        } else {
+            result = getResultIsTriangle(sideA, sideB, sideC, sideAEqualB, sideAEqualC, sideBEqualC);
+        }
+        return result;
+    }
+
+    private static String getResultIsTriangle(int sideA, int sideB, int sideC, boolean sideAEqualB, boolean sideAEqualC, boolean sideBEqualC) {
+        String result;
+        if (sideAEqualB && sideAEqualC) {
+            result = TRIANGLEEQUILATERAL;
+        } else {
+            if ((sideAEqualB && sideA != sideC) || (sideAEqualC && sideA != sideB) || sideBEqualC && sideC != sideA) {
+                result = TRIANGLEISOSCELES;
+            } else {
+                result = NORMALTRIANGLE;
+            }
+        }
+        return result;
+    }
+}
---
+++ test/TriangleClassifierTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleClassifierTest {
+    @Test
+    public void testTriangleEquilateral() {
+        int sideA = 2;
+        int sideB = 2;
+        int sideC = 2;
+
+        String expected = TriangleClassifier.TRIANGLEEQUILATERAL;
+        String result = TriangleClassifier.classifyTriangle(sideA, sideB, sideC);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testTriangleIsoscelesAtBC() {
+        int sideA = 3;
+        int sideB = 2;
+        int sideC = 2;
+
+        String expected = TriangleClassifier.TRIANGLEISOSCELES;
+        String result = TriangleClassifier.classifyTriangle(sideA, sideB, sideC);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testTriangleIsoscelesAtAB() {
+        int sideA = 9;
+        int sideB = 9;
+        int sideC = 7;
+
+        String expected = TriangleClassifier.TRIANGLEISOSCELES;
+        String result = TriangleClassifier.classifyTriangle(sideA, sideB, sideC);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testTriangleIsoscelesAtAC() {
+        int sideA = 5;
+        int sideB = 3;
+        int sideC = 5;
+
+        String expected = TriangleClassifier.TRIANGLEISOSCELES;
+        String result = TriangleClassifier.classifyTriangle(sideA, sideB, sideC);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testTriangleBasic() {
+        int sideA = 3;
+        int sideB = 4;
+        int sideC = 2;
+
+        String expected = TriangleClassifier.NORMALTRIANGLE;
+        String result = TriangleClassifier.classifyTriangle(sideA, sideB, sideC);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testNotTriangle() {
+        int sideA = 4;
+        int sideB = 8;
+        int sideC = 2;
+
+        String expected = TriangleClassifier.NOTTRIANGLE;
+        String result = TriangleClassifier.classifyTriangle(sideA, sideB, sideC);
+        assertEquals(expected, result);
+    }
+}
